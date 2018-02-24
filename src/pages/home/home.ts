e75cc2c34208d9c0eb8a2f46d30a67d7887da155
Pikaa <nws05_01082270@pb.edu.bn>
2018-02-24 14:22:23 +0800

    1

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { women-s-WomenSimportimport { WomenSCategoryPage } from '../women-s-category/women-s-category';
+ { WomenSCategoryPage } from '../women-s-category/women-s-category';
+CategoryPage}
 
 @Component({
   selector: 'page-home',
@@ -10,5 +13,9 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
+
+  gotowomen-s-category(){
+    this.navCtrl.push(WomenSCategoryPage);
+  }  
 
 }
