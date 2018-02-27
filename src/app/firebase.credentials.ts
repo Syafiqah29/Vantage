33943a82bae9b3dbb9f6b4eb00a1293e3d151559
Pikaa <nws05_01082270@pb.edu.bn>
2018-02-27 15:16:17 +0800

    firebase

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyBs_7vackdKKp3sP8dcoqMG7YPIuwfuEzc",
+    authDomain: "vantage-2018.firebaseapp.com",
+    databaseURL: "https://vantage-2018.firebaseio.com",
+    projectId: "vantage-2018",
+    storageBucket: "",
+    messagingSenderId: "391351072737"
+  };
