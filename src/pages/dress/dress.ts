f663abe430681a68c9a8a0ea23f4270e94c31784
Hafiz <nws05_01012429@pb.edu.bn>
2018-03-04 20:17:38 +0800

    last

--- src/pages/dress/dress.ts
+++ src/pages/dress/dress.ts
@@ -24,7 +24,7 @@ export class DressPage {
     // this.navCtrl.setRoot(DressPage);
     // this.http.get('gs://vantage-2018.appspot.com/15276618_1810531539202207_8823075540064796672_n.jpg').map(res => res.json).subscribe(data => {
     //   this.posts = data.data.children;
-  });
+ 
   }
 
   ionViewDidLoad() {
